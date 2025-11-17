c48f6b73ad8243226f1f7a599181b9ddd1c01b7d
Aman sah <amanrck96@gmail.com>
2025-11-17 13:57:22 +0000

    update the the contact us page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -100,6 +100,24 @@ export default function ContactPage() {
                   </div>
                 </CardContent>
               </Card>
+               <Card className="mt-8">
+                <CardHeader>
+                  <CardTitle>Our Location</CardTitle>
+                </CardHeader>
+                <CardContent>
+                   <div className="aspect-video w-full rounded-lg overflow-hidden">
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.9045763953507!2d77.61623867601057!3d12.97792248733737!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae168536b85675%3A0xbef91957c5a0833a!2sRBANM&#39;s%20First%20Grade%20College!5e0!3m2!1sen!2sin!4v1763784013444!5m2!1sen!2sin"
+                      width="100%"
+                      height="100%"
+                      style={{ border: 0 }}
+                      allowFullScreen={false}
+                      loading="lazy"
+                      referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                  </div>
+                </CardContent>
+              </Card>
             </div>
             <div>
               <Card>
